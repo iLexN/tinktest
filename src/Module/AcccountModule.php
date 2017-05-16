f9a43d0fae9f07972c5560fe9a28b1cff595c945
iLexN <ilex.job@gmail.com>
2017-05-16 21:31:08 +0800

    fix test

--- src/Module/AcccountModule.php
+++ src/Module/AcccountModule.php
@@ -71,7 +71,7 @@ class AcccountModule
      */
     private function getValidatorField()
     {
-        return ['name', 'balance', 'status'];
+        return ['name', 'amount', 'status'];
     }
 
     /**
